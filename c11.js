b19a278650ce6ef2e3c23d0f99b2bad4f98a0bc3
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-03 08:03:44 +0700

    Revisi tugas 11

--- c11.js
+++ c11.js
@@ -1,44 +1,30 @@
 const fs = require("fs");
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout, prompt: "Tebakan: "
-});
-
+const readline = require("readline");
 const data = fs.readFileSync("./data.json", "utf-8");
-
+const rl = readline.createInterface({input: process.stdin, output: process.stdout, prompt: "Tebakan: "});
 const obj = JSON.parse(data);
-obj[2] = {"definition": "Sebutkan kota yang memiliki julukan kota Intan?", "term": "Garut"};
+let count = 0; counter = 0;
 
-fs.writeFileSync("./data.json", JSON.stringify(obj),"utf-8")
+obj.push({"definition":"Sebutkan kota yang memiliki julukan kota Intan?","term":"Garut"});
+fs.writeFileSync("./data", JSON.stringify(obj));
 
-let count = 0;
-
-console.log("selamat datang di permainan Tebak Kata, silahkan isi jawaban yang benar ya! \n");
-
-console.log("Pertanyaan: " + obj[count].definition);
-
+console.log("Selamat datang di Tebak kata, silahkan isi jawaban yang benar ya! \n")
+console.log("Pertanyaan:", obj[count].definition);
 rl.prompt();
 
 rl.on("line", (answer) => {
-
-    if (answer.toString().toLowerCase() == obj[count].term.toLowerCase()) {
+    if(answer.toString().toLowerCase() == obj[count].term.toLowerCase()) {
         console.log("Selamat Anda Benar! \n");
         count++;
         if(count == obj.length) {
-            console.log("Hore Anda Menang!");
-            rl.close();
-        };
-        
-
+            console.log("Hore Anda Menang!")
+            rl.close()
+        }
+        console.log("Pertanyaan:", obj[count].definition);
     } else {
-        console.log("Wkwkwkwk, Anda Kurang Beruntung! \n")
+        console.log("Wkwkwkwk, Anda kurang beruntung! \n")
     }
-    console.log(obj[count].definition);
     rl.prompt();
-
 }).on("close", () => {
-    process.exit(0);
+    process.exit(0)
 });
-
-
