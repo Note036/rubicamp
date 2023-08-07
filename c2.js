177dfd183c7c5047921507b3b992478803317d7b
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-07 19:28:32 +0700

    Revisi tugas 2

--- c2.js
+++ c2.js
@@ -1,20 +1,11 @@
 function deretKaskus(n) {
-    const deret = [];
-    let l = n * 3;
-    for(let i = 3; i <= l; i += 3) {
-        if(i % 5 == 0 && i % 6 == 0) {
-            deret.push("KASKUS")
-        } else if(i % 5 == 0) {
-            deret.push("KAS")
-        } else if(i % 6 == 0) {
-            deret.push("KUS")    
-        } else {
-            deret.push(i)
-        }
-        
-    };
-     
-    return deret
+    let deret = [], batas = n * 3
+    for (let i = 3; i <= batas; i += 3) {
+        if (i % 5 == 0 && i % 6 == 0) deret.push("KasKus")
+        else if (i % 5 == 0) deret.push("Kas")
+        else if (i % 6 == 0) deret.push("Kus")
+        else deret.push(i)
+    } return deret
 }
 
- console.log(deretKaskus(10));
+console.log(deretKaskus(10))
