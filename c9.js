cac0c022e13c1e9936845b19a949c9ed6fe2eeb1
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-07 19:30:54 +0700

    Revisi tugas 9

--- c9.js
+++ c9.js
@@ -1,62 +1,29 @@
 function spiral(n) {
     const matrix = [];
-    const arrSpiral = [];
+    const result = [];
+    let firstRow = 0, lastRow = n - 1, firstColumn = 0, lastColumn = n - 1;
+    let nilai = 0, counter = 0;
 
-    for(let i = 0; i < n; i++) {
-        matrix.push([]);
-    };
-     
-     let nilai = 0;
-     let startRow = 0;
-     let endRow = n;
-     let startColumn = 0;
-     let endColumn =  n;
-    
-    while (startRow < endRow) {
-        for(let i = startColumn; i < endColumn; i++) {
-            matrix[startRow][i] = nilai;
+    for (let i = 0; i < n; i++) matrix.push([]);
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            matrix[i][j] = nilai;
             nilai++;
-        };
-        startRow++;
-        
-    }
-    
-    let firstRow = 0;
-    let edgeRow = n - 1;
-    let firstColumn = 0;
-    let edgeColumn = n - 1;
-    
-    while (firstColumn <= edgeColumn && firstRow <= edgeRow) {
-
-    for (let i = firstColumn; i <= edgeColumn; i++) {
-        arrSpiral.push(matrix[firstColumn][i])
-    }
-    
-    firstRow++;
-
-    for (let i = firstRow; i <= edgeRow; i++) {
-        arrSpiral.push(matrix[i][edgeColumn]);  
+        }
     }
 
-    edgeColumn--;
-
-    for (let i = edgeColumn; i >= firstColumn; i--) {
-        arrSpiral.push(matrix[edgeRow][i]);
-    }
-
-    edgeRow--;
-    
-    for (let i = edgeRow; i >= firstRow; i--) {
-        arrSpiral.push(matrix[i][firstColumn]);
-    }
-    
-    firstColumn++;
-
-    }
-    return arrSpiral
-    
+    while (firstColumn <= lastColumn && firstRow <= lastRow) {
+        for (let i = firstColumn; i <= lastColumn; i++) result.push(matrix[firstRow][i])
+        firstRow++
+        for (let i = firstRow; i <= lastRow; i++) result.push(matrix[i][lastColumn])
+        lastColumn--
+        for (let i = lastColumn; i >= firstColumn; i--) result.push(matrix[lastRow][i])
+        lastRow--
+        for (let i = lastRow; i >= firstRow; i--) result.push(matrix[i][firstColumn])
+        firstColumn++
+    } console.log(result)
 }
 
-console.log(spiral(5));
-console.log(spiral(6));
-console.log(spiral(7));
+spiral(5);
+spiral(6);
+spiral(7);
