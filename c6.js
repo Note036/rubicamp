69bc7c8261d7158a303c13f0bf3fc32de3ed2349
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-07 19:30:12 +0700

    Revisi tugas 6

--- c6.js
+++ c6.js
@@ -1,21 +1,9 @@
-function stringManipulation(word) {
-    const pisahKata = word.split(" ");
-    const vokal = "aiueo";
-    let txt = ""
-    for(let i = 0; i < pisahKata.length; i++) {
-        if(vokal.includes(pisahKata[i].charAt(0)) == true) {
-            txt += pisahKata[i] + " ";
-            console.log(pisahKata[i])
-        };
-        if(vokal.includes(pisahKata[i].charAt(0)) == false) {
-            txt += pisahKata[i].slice(1) + pisahKata[i].charAt(0) + "nyo ";
-        };
-    }; 
-    return console.log(txt);
-};
+function sentenceManipulation(sentence) {
+    const kalimat = [], kata = sentence.split(" ")
+    for(let i = 0; i < kata.length; i++) {
+        if(/^[aiueo]/.test(kata[i])) kalimat.push(kata[i])
+        else kalimat.push(kata[i].slice(1) + kata[i][0] +"nyo")
+    } console.log(kalimat.join(" "))
+}
 
-stringManipulation("ibu pergi ke pasar bersama aku");
-
-// let a = "ibu pergi ke pasar bersama aku";
-// let b = a.split(" ");
-// console.log(b)
+sentenceManipulation("ibu pergi ke pasar bersama aku")
