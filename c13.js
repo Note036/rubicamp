c6971a2aaadfdddce0148a8eade1858e14524c3f
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-07 19:33:18 +0700

    Revisi tugas 13

--- c13.js
+++ c13.js
@@ -1,104 +1,103 @@
 const fs = require("fs");
 const data = fs.readFileSync("./toDo.json", "utf-8");
 const obj = JSON.parse(data);
-let inform = process.argv[3];
-let command = process.argv[2];
-let objIndex = obj[obj.findIndex(x => x.ID == inform)];
-let count = obj.length + 1
+const command = process.argv[2], id = process.argv[3], inform = process.argv.slice(3).join(" ");
+let objIndex = id -1, count = obj.length + 1;
 
 if (!command || command.toLowerCase() == "help") {
-    console.log(`>>> JS RODO <<<
-node todo.js list
-node todo.js task <task_id>
-node todo.js add <task_content>
-node todo.js delet <task_id>
-node todo.js complete <task_id>
-node todo.js uncomplete <task_id>
-node todo.js list:outstanding asc|desc
-node todo.js list:completed asc|desc
-node todo.js tage <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
-node todo.js filter:<tag_name>`)
-}
-else {
+    console.log(`>>> JS TODO <<<
+node c13.js <command>
+node c13.js list
+node c13.js task <task_id>
+node c13.js add <task_conten>
+node c13.js delete <task_id>
+node c13.js complete <task_id>
+node c13.js uncomplete <task_id>
+node c13.js list:outstanding asc|desc
+node c13.js list:completed asc|desc
+node c13.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
+node c13.js filter:<tag_name>`)
+} else {
     switch (command.toLowerCase()) {
         case "list":
-            console.log("Daftar Pekerjaan");
-            for (let i = 0; i < obj.length; i++) {
-                if (obj[i].complete) {
-                    obj[i].complete = "[x]";
-                    console.log(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}.`);
-                } else if (!obj[i].complete) {
-                    obj[i].complete = "[ ]";
-                    console.log(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}.`);
+            console.log("Daftar Pekerjaan")
+            for (let x of obj) {
+                if (x.complete) {
+                    x.complete = "[x]"
+                    console.log(`${x.ID}. ${x.complete} ${x.title}.`)
+                } else if (!x.complete) {
+                    x.complete = "[ ]"
+                    console.log(`${x.ID}. ${x.complete} ${x.title}.`)
                 }
             }; break;
         case "task":
-            let tag = []
-            let tag2 = objIndex
-            for (let x in tag2) tag.push(x + ": " + objIndex[x]);
-            console.log(tag.join("\n"))
+            for(let x in obj[objIndex]) console.log(`${x}: ${obj[id-1][x]}`);
             break;
         case "add":
-            if (process.argv.slice(3).join(" ")) {
-                console.log(`"${process.argv.slice(3).join(" ")}" telah diambahkan.`);
-                obj.push({ "ID": count, "title": process.argv.slice(3).join(" "), "complete": false, "tags": "" });
-                fs.writeFileSync("./toDo.json", JSON.stringify(obj));
-            } else if (!process.argv.slice(3).join(" ") || process.argv.slice(3).join(" ") == " ") return
+            if (inform) {
+                console.log(`"${inform}" telah ditambahkan.`);
+                obj.push({ "ID": count, "title": inform, "complete": false, tag: "" });
+                fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8")
+            } else if(!inform || inform == " ") return
             break;
-        case "delet":
-            console.log(`"${objIndex.title}" telah dihapus dari daftar.`);
-            obj.splice(obj.findIndex(x => x.ID == inform), 1);
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj));
+        case "delete":
+            console.log(`"${obj[id-1].title}" telah dihapus dari daftar.`);
+            obj.splice(objIndex, 1);
+            for (let i = 0; i < obj.length; i++) obj[i].ID = i + 1
+            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
             break;
         case "complete":
-            console.log(`"${objIndex.title}" telah selesai.`);
-            objIndex.complete = true;
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj));
+            console.log(`"${obj[id - 1].title}" telah selesai.`);
+            obj[id-1].complete = true;
+            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
             break;
         case "uncomplete":
-            console.log(`"${objIndex.title}" status selesai dibatalkan.`);
-            objIndex.complete = false
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj));
+            console.log(`"${obj[id-1].title}" status selesai dibatalkan.`);
+            obj[objIndex].complete = false;
+            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
             break;
         case "list:outstanding":
-            console.log("Daftar Pekerjaan");
-            let txt = [];
-            for (let i = 0; i < obj.length; i++) {
-                if (!obj[i].complete) {
-                    obj[i].complete = "[ ]"
-                    txt.push(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}.`);
+            console.log("Daftar Pekerjaan")
+            let outstanding = [];
+            for (let x of obj) {
+                if (!x.complete) {
+                    x.complete = "[ ]";
+                    outstanding.push(`${x.ID}. ${x.complete} ${x.title}.`);
                 }
-            } if (inform == "asc") console.log(txt.join("\n"));
-            else if (inform == "desc") console.log(txt.reverse().join("\n"))
+            }
+            if (id == "asc") console.log(outstanding.join("\n"));
+            else if (id == "desc") console.log(outstanding.reverse().join("\n"));
             break;
         case "list:completed":
-            console.log("Daftar Pekerjaan");
-            let txt1 = [];
-            for (let i = 0; i < obj.length; i++) {
-                if (obj[i].complete) {
-                    obj[i].complete = "[x]"
-                    txt1.push(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}.`);
+            console.log("Daftar Pekerjaan")
+            let completed = [];
+            for (let x of obj) {
+                if (x.complete) {
+                    x.complete = "[x]";
+                    completed.push(`${x.ID}. ${x.complete} ${x.title}.`);
                 }
-            } if (inform == "asc") console.log(txt1.join("\n"));
-            else if (inform == "desc") console.log(txt1.reverse().join("\n"))
+            }
+            if (id == "asc") console.log(completed.join("\n"));
+            else if (id == "desc") console.log(completed.reverse().join("\n"));
             break;
-        case "tags":
-            console.log(`Tag '${process.argv.slice(4).join(',')}' telah ditambahkan ke daftar '${objIndex.title}'`)
-            objIndex.tags = process.argv.slice(4);
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj));
+        case "tag":
+            console.log(`Tag "${process.argv.slice(4)}" telah ditambahkan ke dalam daftar '${obj[obj.findIndex(x => x.ID == id)].title}'`);
+            obj[objIndex].tag = process.argv.slice(4);
+            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
             break;
-        // case command:
-        //     console.log("Daftar Pekerjaan");
-        //     for (let i = 0; i < obj.length; i++) {
-        //         if (obj[i].tags.includes(command.split(":")[1])) {
-        //             if (obj[i].complete) {
-        //                 obj[i].complete = "[x]";
-        //                 console.log(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}`)
-        //             } else if (!obj[i].complete) {
-        //                 obj[i].complete = "[ ]";
-        //                 console.log(`${obj[i].ID}. ${obj[i].complete} ${obj[i].title}`)
-        //             }
-        //         }
-        //     }
+        case `filter:${command.slice(7)}`:
+            console.log("Daftar Pekerjaan");
+            console.log(command.slice(7));
+            for (let x of obj) {
+                if (x.tag.includes(command.slice(7))) {
+                    if (x.complete) {
+                        x.complete = "[x]"
+                        console.log(`${x.ID}. ${x.complete} ${x.title}.`)
+                    } else if (!x.complete) {
+                        x.complete = "[ ]"
+                        console.log(`${x.ID}. ${x.complete} ${x.title}.`)
+                    }
+                }
+            }
     }
 }
