1456eff36729560d7c2de8eaa32a4d85d58d41f5
Ramdani <ramdani@ramdani-ThinkPad-T495.(none)>
2023-08-07 19:30:33 +0700

    Revisi tugas 8

--- c8.js
+++ c8.js
@@ -1,27 +1,11 @@
 function pola(str) {
-    
-    const strToArr = str.split(" ");
-    let x = strToArr[0];
-    let y = strToArr[2];
-    let z = strToArr[4];
-    const arrResult = [];
-
-    for(let i = 0; i <= 9; i++) {
-        for(let j = 0; j <= 9; j++) {
-            let zBaru = z.replace("#", i);
-            let xBaru = x.replace("#", j);   
-            if(xBaru * y == zBaru) {
-                arrResult.push(j);
-                arrResult.push(i);
-                return arrResult
-            }
-             
-            }; 
-
-     }     
-   
+    const angka = str.split(" ")
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (angka[0].replace("#", i) * angka[2] == angka[4].replace("#", j)) return [i, j]
+        }
+    }
 }
 
 console.log(pola("42#3 * 188 = 80#204"));
 console.log(pola("8#61 * 895 = 78410#5"));
-
